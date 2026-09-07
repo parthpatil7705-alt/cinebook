dc16bc4be479db2d47efb0399cccb4250737ce32
parthpatil7705-alt <parthpatil7705@gmail.com>
2026-09-07 22:00:38 +0530

    Fix Vercel pip install PEP-668 error by using venv

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "Setting up virtual environment..."
+python3 -m venv venv
+source venv/bin/activate
+
 echo "Installing dependencies..."
 python3 -m pip install -r requirements.txt
 
