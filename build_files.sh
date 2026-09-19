402e6572f886073b397775d1161ef50ac0e6e979
parthpatil7705-alt <parthpatil7705@gmail.com>
2026-09-19 12:12:10 +0530

    Simplify Vercel config - let Django serve static files

--- build_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Installing dependencies..."
-pip install --break-system-packages -r requirements.txt
-
-echo "Collecting static files..."
-python3 manage.py collectstatic --noinput
