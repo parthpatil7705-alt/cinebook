12ddbed32bafdca433213c8840238f4198138b4d
parthpatil7705-alt <parthpatil7705@gmail.com>
2026-09-19 10:47:16 +0530

    Remove migrations from build - run locally instead

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-echo "Setting up virtual environment..."
-python3 -m venv venv
-source venv/bin/activate
-
 echo "Installing dependencies..."
-python3 -m pip install -r requirements.txt
+pip install -r requirements.txt
 
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput
-
-echo "Running migrations..."
-python3 manage.py migrate --noinput
